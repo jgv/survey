13afe540fb39a876d41354e1eef2e64548cf89ca
jgv <jvingiano@gmail.com>
2011-06-06 02:30:42 -0400

    cleaning up

--- grab.js
+++ grab.js
@@ -1,3 +1,8 @@
+/*
+ * Jonathan Vingiano and Brad Troemel, 2011
+ *
+ */
+
 window.onload = (function () {
     if (window == top) {
         var foundImages = scrape();
@@ -6,14 +11,17 @@ window.onload = (function () {
 
     function scrape() {
         var foundImages = [],
-        images = document.body.getElementsByTagName('img');
+        images = document.body.getElementsByTagName('img'),
+        re = /(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*\.(?:jpe?g|gif|png))(?:\?([^#]*))?(?:#(.*))?/i;
         
         if (images.length === 0) {
             return;
         }
         
-        for (i = 0, len = images.length; i < len; i++) {
-            foundImages.push(images[i].src);
+        for (i = 0, len = images.length; i < len; i++) {            
+            if (re.test(images[i].src)) {
+                foundImages.push(encodeURIComponent(images[i].src));
+            }
         }
         
         return foundImages;
