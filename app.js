db6e9e5cf328657845846a6d5b7f2b6c23e7672f
jgv <jvingiano@gmail.com>
2011-06-06 02:30:36 -0400

    using xhr

--- app.js
+++ app.js
@@ -4,8 +4,21 @@
  */
 
 chrome.extension.onRequest.addListener(
-    function(foundImages, sender, sendResponse){
-        console.log(foundImages);
+    function(foundImages, sender, sendResponse){      
+        for (i = 0, len = foundImages.length; i < len; i++) {
+        var url = "http://severe-frost-512.heroku.com/create",
+            params = "url=" + foundImages[i],
+            xhr = new XMLHttpRequest();
+        
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function() {//Call a function when the state changes.
+	              if(xhr.readyState == 4 && xhr.status == 200) {
+		                console.log(xhr.responseText);
+	              }
+            }            
+            xhr.send(params);
+        }
         sendResponse();
     }
 );
