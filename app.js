2a22df6b870b5dc830c2ece93c6419f993e6a932
jgv <jvingiano@gmail.com>
2011-06-06 03:10:19 -0400

    checking for images before operating on them;

--- app.js
+++ app.js
@@ -5,20 +5,22 @@
 
 chrome.extension.onRequest.addListener(
     function(foundImages, sender, sendResponse){      
-        for (i = 0, len = foundImages.length; i < len; i++) {
-        var url = "http://severe-frost-512.heroku.com/create",
-            params = "url=" + foundImages[i],
-            xhr = new XMLHttpRequest();
-        
-            xhr.open('POST', url, true);
-            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhr.onreadystatechange = function() {//Call a function when the state changes.
-	              if(xhr.readyState == 4 && xhr.status == 200) {
-		                console.log(xhr.responseText);
-	              }
-            }            
-            xhr.send(params);
+        if (foundImages) {
+            for (i = 0, len = foundImages.length; i < len; i++) {
+                var url = "http://severe-frost-512.heroku.com/create",
+                    params = "url=" + foundImages[i],
+                    xhr = new XMLHttpRequest();
+                
+                xhr.open('POST', url, true);
+                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhr.onreadystatechange = function() {//Call a function when the state changes.
+	                  if(xhr.readyState == 4 && xhr.status == 200) {
+		                    console.log(xhr.responseText);
+	                  }
+                }            
+                xhr.send(params);
+            }
+            sendResponse();
         }
-        sendResponse();
     }
 );
