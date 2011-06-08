5114a000f95cb6bd089920a306b53fdaacec500f
jgv <jvingiano@gmail.com>
2011-06-07 21:35:55 -0400

    supporting the source of the url as a parameter

--- app.js
+++ app.js
@@ -6,13 +6,21 @@
 chrome.extension.onRequest.addListener(
     function(foundImages, sender, sendResponse){      
         if (foundImages) {
-            for (i = 0, len = foundImages.length; i < len; i++) {
-                var url = "http://severe-frost-512.heroku.com/create",
-                    params = "url=" + foundImages[i],
-                    xhr = new XMLHttpRequest();
+            var url = "http://severe-frost-512.heroku.com/create";
                 
+            var source = foundImages.pop();
+            
+            for (i = 0, len = foundImages.length; i < len; i++) {            
+                console.log(i);
+                var xhr = new XMLHttpRequest(),
+                    img = foundImages[i],
+                    params = 'url=' + img + '&source=' + source;
+                
+                console.log(img);
+
                 xhr.open('POST', url, true);
-                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
                 xhr.onreadystatechange = function() {//Call a function when the state changes.
 	                  if(xhr.readyState == 4 && xhr.status == 200) {
 		                    console.log(xhr.responseText);
--- grab.js
+++ grab.js
@@ -4,27 +4,30 @@
  */
 
 (function () {
+    if (window.location.href !== 'http://severe-frost-512.heroku.com/list') {
 
-    var foundImages = scrape();
-    chrome.extension.sendRequest(foundImages);
+        var foundImages = scrape();
+        chrome.extension.sendRequest(foundImages);
 
-
-    function scrape() {
-        var foundImages = [],
-        images = document.body.getElementsByTagName('img'),
-        re = /(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*\.(?:jpe?g|gif|png))(?:\?([^#]*))?(?:#(.*))?/i;
-        
-        if (images.length === 0) {
-            return;
-        }
-        
-        for (i = 0, len = images.length; i < len; i++) {            
-            if (re.test(images[i].src)) {
-                foundImages.push(images[i].src);
+        function scrape() {
+            var foundImages = [],
+            images = document.body.getElementsByTagName('img'),
+            re = /(http(s?):)|([\/|.|\w|\s])*\.(?:jpg|gif|png)/i;
+            
+            if (images.length === 0) {
+                return;
+            }            
+            
+            for (i = 0, len = images.length; i < len; i++) {            
+                if (re.test(images[i].src)) {
+                    foundImages.push(images[i].src);
+                }
             }
+
+            foundImages.push(window.location.href); // last item is source
+            console.log(foundImages);
+            
+            return foundImages;
         }
-        console.log(foundImages);
-        
-        return foundImages;
     }
 })();
